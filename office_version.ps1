e2c5931f5804e05cdedde89c9c054a4e086fd0bd
Don Fernandes <don@cbctech.net>
2024-01-10 13:15:55 -0500

    Add files via upload

---
+++ office_version.ps1
@@ -0,0 +1,22 @@
+﻿#requires -Version 1.0
+<#
+    .SYNOPSIS
+    gets currently installed version of Microsoft Office
+
+    .DESCRIPTION
+    gets M365 version and outputs to text file
+
+    .USAGE
+
+    .LINK
+
+    .NOTES
+    Don Fernandes
+    don@cbctech.net
+    20240110 created
+
+#>
+
+Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration' |
+Select-Object -ExpandProperty VersionToReport |
+Out-File -FilePath 'C:\tech_stuff\office_version.txt' -Force
